c6caed2a3ea15a4aa62f8db48431afa90d3c9683
kaiserkey <kaiserkey2@gmail.com>
2023-03-24 23:28:13 -0300

    new changes

--- Controllers/InmuebleController.cs
+++ Controllers/InmuebleController.cs
@@ -4,25 +4,5 @@ using Inmobiliaria.Models;
 
 namespace Inmobiliaria.Controllers
 {
-    [Route("[controller]")]
-    public class InmuebleController : Controller
-    {
-        private readonly ILogger<InmuebleController> _logger;
-
-        public InmuebleController(ILogger<InmuebleController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View("Error!");
-        }
-    }
+    
 }
