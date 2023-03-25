f23b8e78c40b6552e975808ecee88636b3e976c5
kaiserkey <kaiserkey2@gmail.com>
2023-03-25 00:17:30 -0300

    new changes

--- Models/RepositorioInmueble.cs
+++ Models/RepositorioInmueble.cs
@@ -1,7 +1,7 @@
 using System;
 namespace Inmobiliaria.Models;
 
-public class Repositorio
+public class RepositorioPersona
 {
     private int idInmueble{ get; set; }
     private string tipo{ get; set; }
