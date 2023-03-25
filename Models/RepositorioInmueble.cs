fc26e2840de13fb518fb1236afdee68c992fe095
kaiserkey <kaiserkey2@gmail.com>
2023-03-25 00:17:38 -0300

    new changes

--- Models/RepositorioInmueble.cs
+++ Models/RepositorioInmueble.cs
@@ -3,14 +3,5 @@ namespace Inmobiliaria.Models;
 
 public class RepositorioPersona
 {
-    private int idInmueble{ get; set; }
-    private string tipo{ get; set; }
-    private string coordenadas{ get; set; }
-    private decimal precio{ get; set; }
-    private int ambientes{ get; set; }
-    private string uso{ get; set; }
-    private Boolean activo{ get; set; }
-    private int idPropietario{ get; set; }
-
     
 }
