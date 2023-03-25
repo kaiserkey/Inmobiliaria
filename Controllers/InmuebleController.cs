55c14d544bcb82411348669cb9258f66548a5162
kaiserkey <kaiserkey2@gmail.com>
2023-03-24 23:40:17 -0300

    new changes

--- Controllers/InmuebleController.cs
+++ Controllers/InmuebleController.cs
@@ -8,7 +8,7 @@ namespace Inmobiliaria.Controllers;
 
 public class InmuebleController : Controller
 {
-    var cmd
+    var cmd;
     private MySqlDatabase MySqlDatabase { get; set; }
     public InmuebleController(MySqlDatabase mySqlDatabase)
     {
