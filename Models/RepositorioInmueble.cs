3dca9047729e2bf01d90b2f57b559382d9263d55
kaiserkey <kaiserkey2@gmail.com>
2023-03-25 01:00:56 -0300

    new changes

--- Models/RepositorioInmueble.cs
+++ Models/RepositorioInmueble.cs
@@ -5,11 +5,7 @@ namespace Inmobiliaria.Models;
 
 public class RepositorioInmueble
 {
-    private MySqlDatabase MySqlDatabase { get; set; }
-    public RepositorioInmueble(MySqlDatabase mySqlDatabase)
-    {
-        this.MySqlDatabase = mySqlDatabase;
-    }
+    
 
     public List<Inmueble> GetInmuebles()
     {
