6b092dbebacf7d643a9ec061dbcd98e0918d71af
kaiserkey <kaiserkey2@gmail.com>
2023-03-24 22:17:08 -0300

    new changes

--- Models/MySqlDatabase.cs
+++ Models/MySqlDatabase.cs
@@ -1,12 +1,21 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
-namespace Inmobiliaria.Models
+namespace TextTasks
 {
-    public class MySqlDatabase
+  public class MySqlDatabase : IDisposable
+  {
+    public MySqlConnection Connection;
+
+    public MySqlDatabase(string connectionString)
     {
-        
+      Connection = new MySqlConnection(connectionString);
+      this.Connection.Open();
     }
+
+    public void Dispose()
+    {
+      Connection.Close();
+    }
+  }
 }
