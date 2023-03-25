3a942b18a41546783ae945ba1bf0c8d5cede7850
kaiserkey <kaiserkey2@gmail.com>
2023-03-25 00:16:57 -0300

    new changes

---
+++ Models/Inmueble copy.cs
@@ -0,0 +1,16 @@
+using System;
+namespace Inmobiliaria.Models;
+
+public class Inmueble
+{
+    private int idInmueble{ get; set; }
+    private string tipo{ get; set; }
+    private string coordenadas{ get; set; }
+    private decimal precio{ get; set; }
+    private int ambientes{ get; set; }
+    private string uso{ get; set; }
+    private Boolean activo{ get; set; }
+    private int idPropietario{ get; set; }
+
+    
+}
