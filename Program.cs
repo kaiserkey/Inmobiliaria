396b67c89ceae8cd2d2f6377004eabba7e3632c0
kaiserkey <kaiserkey2@gmail.com>
2023-03-25 01:12:07 -0300

    new changes

--- Program.cs
+++ Program.cs
@@ -8,7 +8,8 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 var serviceProvider = new ServiceCollection();
-
+// Agrega la clase MySqlDatabase al contenedor de servicios.
+    services.AddSingleton<IMySqlDatabase, MySqlDatabase>();
 serviceProvider.AddTransient<MySqlDatabase>(_ => new MySqlDatabase("server=localhost; database=Inmobiliaria; uid=root; pwd=1234;"));
 //.BuildServiceProvider();
 /* Esta linea de codigo se utiliza para crear una instancia de la clase MySqlDatabase 
