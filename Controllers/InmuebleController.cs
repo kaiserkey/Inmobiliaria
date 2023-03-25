3b05dbaca816a084c116821685967663acecf51a
kaiserkey <kaiserkey2@gmail.com>
2023-03-25 17:02:47 -0300

    new changes

--- Controllers/InmuebleController.cs
+++ Controllers/InmuebleController.cs
@@ -34,7 +34,7 @@ public class InmuebleController : Controller
         int res = inmuebles.CreateInmueble(con, inmueble);
         if (res == 0)
         {
-            return View();
+            return View(Inmueble);
         }else 
         {
             return RedirectToAction(nameof(Index));
