67a71251cc0cbc615048e8cae8f567cd35c4dc52
kaiserkey <kaiserkey2@gmail.com>
2023-03-25 00:18:37 -0300

    new changes

--- Controllers/InmuebleController.cs
+++ Controllers/InmuebleController.cs
@@ -1,16 +1,12 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Inmobiliaria.Models;
-using MySql.Data.MySqlClient;
+
 using System.Collections.Generic;
 
 namespace Inmobiliaria.Controllers;
 
 public class InmuebleController : Controller
 {
-    private MySqlDatabase MySqlDatabase { get; set; }
-    public InmuebleController(MySqlDatabase mySqlDatabase)
-    {
-        this.MySqlDatabase = mySqlDatabase;
-    }
+    
 }
--- Models/RepositorioInmueble.cs
+++ Models/RepositorioInmueble.cs
@@ -3,5 +3,9 @@ namespace Inmobiliaria.Models;
 
 public class RepositorioInmueble
 {
-    
+    private MySqlDatabase MySqlDatabase { get; set; }
+    public InmuebleController(MySqlDatabase mySqlDatabase)
+    {
+        this.MySqlDatabase = mySqlDatabase;
+    }
 }
