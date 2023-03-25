c8b610fc8ba5a283704cbe61abeabcd5e762f7fd
kaiserkey <kaiserkey2@gmail.com>
2023-03-25 16:48:58 -0300

    new changes

--- Models/RepositorioInmueble.cs
+++ Models/RepositorioInmueble.cs
@@ -51,7 +51,7 @@ public class RepositorioInmueble
         cmd.Parameters.AddWithValue("@activo", inmueble.activo);
         cmd.Parameters.AddWithValue("@idPropietario", inmueble.idPropietario);
         cmd.ExecuteNonQuery();
-        recs = mySqlDatabase.Dispose();
+        var recs = mySqlDatabase.Dispose();
         if (recs == 1)
             response = "Todo OK";
         else
