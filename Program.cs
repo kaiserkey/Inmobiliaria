eaab3af50edc5d3d4a34cf45c91e4c9a14d36e11
kaiserkey <kaiserkey2@gmail.com>
2023-03-24 22:46:20 -0300

    new changes

--- Program.cs
+++ Program.cs
@@ -16,7 +16,8 @@ La clase MySqlDatabase se utiliza para conectarse a una base de datos MySQL.
 En este caso, se está creando una nueva instancia de esta clase y pasando una cadena 
 de conexión como parámetro en el constructor. La cadena de conexión especifica el 
 servidor MySQL, la base de datos a la que se conectará, el nombre de usuario y la contraseña.
-Al utilizar el método AddTransient, se registra la clase MySqlDatabase como un servicio transient en el contenedor de servicios de la aplicación. Esto significa que cada vez que se solicite una instancia de la clase MySqlDatabase a través del contenedor de servicios, se creará una nueva instancia de la clase.
+Al utilizar el método AddTransient, se registra la clase MySqlDatabase como un servicio transient 
+en el contenedor de servicios de la aplicación. Esto significa que cada vez que se solicite una instancia de la clase MySqlDatabase a través del contenedor de servicios, se creará una nueva instancia de la clase.
 
 La inyección de dependencias es una técnica de programación que se utiliza para separar la creación de objetos y su uso. En este caso, se registra la clase MySqlDatabase en el contenedor de servicios para que pueda ser utilizada por otras clases de la aplicación que dependan de ella, sin tener que preocuparse por crear una instancia de la clase manualmente.
  */
