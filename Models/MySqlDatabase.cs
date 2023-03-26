36cecdac5b65fade9642e52aff88c14f7f5f4a96
kaiserkey <kaiserkey2@gmail.com>
2023-03-26 20:15:02 -0300

    new changes

--- Models/MySqlDatabase.cs
+++ Models/MySqlDatabase.cs
@@ -6,7 +6,7 @@ namespace Inmobiliaria.Models
     public class MySqlDatabase : IDisposable
     {
         public MySqlConnection Connection;
-        public string connectionString = 
+        public string connectionString = "server=localhost;port=3306;database=mydatabase;uid=root;password=1234"
 
         public MySqlDatabase(string connectionString)
         {
