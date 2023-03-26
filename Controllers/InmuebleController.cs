7eefc4e50c7b5ffd620adddba741ea46d018a334
kaiserkey <kaiserkey2@gmail.com>
2023-03-26 20:36:33 -0300

    new changes

--- Controllers/InmuebleController.cs
+++ Controllers/InmuebleController.cs
@@ -21,7 +21,7 @@ namespace Inmobiliaria.Controllers
             return View(listaInmuebles);
         }
 
-        dotnet-aspnet-codegenerator view Index List -outDir "Views/Inmuebles" -udl --model Inmobiliaria.Models.Inm -f
+        dotnet-aspnet-codegenerator view Index List -outDir "Views/Inmuebles" -udl --model Inmobiliaria.Models.Inmueble -f
 
         // GET: Inmueble/Details/5
         public ActionResult Details(int id)
