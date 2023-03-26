876f17fbf6c9a19ed6f13f26585eae30bdff6455
kaiserkey <kaiserkey2@gmail.com>
2023-03-26 20:19:21 -0300

    new changes

--- Controllers/InmuebleController.cs
+++ Controllers/InmuebleController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using 
+using Inmobiliaria.Models;
 
 namespace Inmobiliaria.Controllers
 {
