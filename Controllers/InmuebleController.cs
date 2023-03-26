943c703da3f772feec243310e02743ce10b8412b
kaiserkey <kaiserkey2@gmail.com>
2023-03-26 20:32:58 -0300

    new changes

--- Controllers/InmuebleController.cs
+++ Controllers/InmuebleController.cs
@@ -17,7 +17,7 @@ namespace Inmobiliaria.Controllers
         public ActionResult Index()
         {
             RepositorioInmueble repositorio = new RepositorioInmueble(con);
-            var lista = repositorio.ObtenerTodos();
+            var lista = repositorio.GetInmuebles();
             return View();
         }
 
