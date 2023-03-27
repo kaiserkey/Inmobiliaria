f46b87f79d42613e4ecdb021c814c6c1d635e2b0
kaiserkey <kaiserkey2@gmail.com>
2023-03-27 09:43:30 -0300

    new changes

--- Controllers/InmuebleController.cs
+++ Controllers/InmuebleController.cs
@@ -57,8 +57,8 @@ namespace Inmobiliaria.Controllers
         // GET: Inmueble/Edit/5
         public ActionResult Edit(int id)
         {
-            Inmueble RepoInmueble.GetInmueble(con, id);
-            return View();
+            Inmueble inmueble = RepoInmueble.GetInmueble(con, id);
+            return View(inmueble);
         }
 
         // POST: Inmueble/Edit/5
