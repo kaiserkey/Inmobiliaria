54f5e56d0e6562bcbf0e71c276f9f17b8fab2f27
kaiserkey <kaiserkey2@gmail.com>
2023-04-08 19:52:44 -0300

    new changes

--- Models/Propietario.cs
+++ Models/Propietario.cs
@@ -9,7 +9,7 @@ public class Propietario
     public string Nombre{ get; set; }
     public string Apellido{ get; set; }
     public string Direccion{ get; set; }
-    public string Dni{ get; set; }
     public string Telefono{ get; set; }
+    public string Dni{ get; set; }
     public string Email{ get; set; }
 }
