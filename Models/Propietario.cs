b2566f1456a29695fbd9a840377d1d4595b06895
kaiserkey <kaiserkey2@gmail.com>
2023-04-08 19:50:26 -0300

    new changes

---
+++ Models/Propietario.cs
@@ -0,0 +1,22 @@
+using System;
+namespace Inmobiliaria.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Inmueble
+{
+    public int IdInmueble{ get; set; }
+    public string Tipo{ get; set; }
+    public string Coordenadas{ get; set; }
+    public decimal Precio{ get; set; }
+    public int Ambientes{ get; set; }
+    public string Uso{ get; set; }
+    public Boolean Activo{ get; set; }
+
+    [Display( Name = "Propietario" ) ]
+    public int IdPropietario{ get; set; }
+
+    /* [ForeignKey( nameof( IdPropietario ) ) ]
+
+    public Propietario Propietario{ get; set; } */
+}
