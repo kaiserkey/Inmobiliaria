2d922915157be4c292fb7bcfcca9f9af4b86256c
kaiserkey <kaiserkey2@gmail.com>
2023-04-08 19:51:21 -0300

    new changes

--- Models/Propietario.cs
+++ Models/Propietario.cs
@@ -5,5 +5,11 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 public class Propietario
 {
-    
+    public int IdPropietario{ get; set; }
+    public string Nombre{ get; set; }
+    public string Apellido{ get; set; }
+    public string Dni{ get; set; }
+    public string Telefono{ get; set; }
+    public string Email{ get; set; }
+    public string Clave{ get; set; }
 }
