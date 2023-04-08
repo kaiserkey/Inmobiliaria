89e78d70d5feecbd5b55245766ea5d8a892383e1
kaiserkey <kaiserkey2@gmail.com>
2023-04-08 19:42:05 -0300

    new changes

--- Models/Inmueble.cs
+++ Models/Inmueble.cs
@@ -11,7 +11,7 @@ public class Inmueble
     public string Uso{ get; set; }
     public Boolean Activo{ get; set; }
 
-    [DisplayName ]
+    [Display() ]
     public int IdPropietario{ get; set; }
 
 
