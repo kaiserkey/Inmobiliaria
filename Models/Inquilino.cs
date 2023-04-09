2393167d40c581e63b51209f79edd60bae5ef623
kaiserkey <kaiserkey2@gmail.com>
2023-04-08 22:52:05 -0300

    new changes

--- Models/Inquilino.cs
+++ Models/Inquilino.cs
@@ -1,12 +1,22 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+namespace Inmobiliaria.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Inmobiliaria.Models
+public class Propietario
 {
-    public class Inquilino
-    {
-        
-    }
+    [Display (Name = "Código")]
+    public int IdPropietario{ get; set; }
+    [Display (Name = "Nombre")]
+    public string Nombre{ get; set; }
+    [Display (Name = "Apellido")]
+    public string Apellido{ get; set; }
+    [Display (Name = "Dirección")]
+    public string Direccion{ get; set; }
+    [Display (Name = "Teléfono")]
+    public string Telefono{ get; set; }
+    [Display (Name = "DNI")]
+    public string Dni{ get; set; }
+    [Display (Name = "Email")]
+    public string Email{ get; set; }
 }
