98b2542a1576cc2e0c73d44610c6a6e225c50e7e
kaiserkey <kaiserkey2@gmail.com>
2023-04-08 23:59:26 -0300

    new changes

--- Controllers/InquilinoController.cs
+++ Controllers/InquilinoController.cs
@@ -50,6 +50,7 @@ namespace Inmobiliaria.Controllers
             {   
                 string fechaNacimientoStr = inquilino.FechaNacimiento.ToString("yyyy-MM-dd HH:mm:ss");
                 DateTime fechaNacimiento = DateTime.ParseExact(fechaNacimientoStr, "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+                //
                 string fechaNacimientoFormateada = fechaNacimiento.ToString("yyyy-MM-dd HH:mm:ss");
 
                 RepoInquilino.CreateInquilino(con, inquilino);
