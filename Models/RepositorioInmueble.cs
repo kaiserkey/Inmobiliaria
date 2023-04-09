8dfd3a136b1ae4d7bcfdc1733c7c2d52e9a0974b
kaiserkey <kaiserkey2@gmail.com>
2023-04-09 20:14:39 -0300

    new changes

--- Models/RepositorioInmueble.cs
+++ Models/RepositorioInmueble.cs
@@ -14,7 +14,7 @@ public class RepositorioInmueble
         var inmuebles = new List<Inmueble>();
         var cmd = mySqlDatabase.Connection.CreateCommand() as MySqlCommand;
         cmd.CommandText = @"SELECT IdInmueble, Tipo, Coordenadas, Precio, Ambientes, Uso, Activo, IdPropietario,
-                         
+                            p.NOmbre
                             FROM Inmueble i INNER JOIN Propietario p ON i.IdPropietario = p.IdPropietario";
 
         using (var reader = cmd.ExecuteReader())
