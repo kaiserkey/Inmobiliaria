a9224f81b58dbbe81e333b9394ac081c22165050
kaiserkey <kaiserkey2@gmail.com>
2023-04-08 21:57:26 -0300

    new changes

--- Models/RepositorioPropietario.cs
+++ Models/RepositorioPropietario.cs
@@ -82,7 +82,7 @@ public class RepositorioPropietario
         cmd.Parameters.AddWithValue("@Email", Propietario.Email);
 
         var res = Convert.ToInt32(cmd.ExecuteScalar());
-
+        Console.WriteLine("Entro Update");
         mySqlDatabase.Dispose();
         
         return res;
