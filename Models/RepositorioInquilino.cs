af374b123056b94fd3109bde0a19dcdc7c10dc79
kaiserkey <kaiserkey2@gmail.com>
2023-04-08 23:12:41 -0300

    new changes

--- Models/RepositorioInquilino.cs
+++ Models/RepositorioInquilino.cs
@@ -24,7 +24,7 @@ public class RepositorioInquilino
                     IdInquilino = reader.GetInt32(nameof(Inquilino.IdInquilino)),
                     Nombre = reader.GetString(nameof(Inquilino.Nombre)),
                     Apellido = reader.GetString(nameof(Inquilino.Apellido)),
-                    Direccion = reader.GetString(nameof(Inquilino.Direccion)),
+                    
                     Telefono = reader.GetString(nameof(Inquilino.Telefono)),
                     Dni = reader.GetString(nameof(Inquilino.Dni)),
                     Email = reader.GetString(nameof(Inquilino.Email)),
@@ -54,7 +54,6 @@ public class RepositorioInquilino
                     IdInquilino = reader.GetInt32(nameof(Inquilino.IdInquilino)),
                     Nombre = reader.GetString(nameof(Inquilino.Nombre)),
                     Apellido = reader.GetString(nameof(Inquilino.Apellido)),
-                    Direccion = reader.GetString(nameof(Inquilino.Direccion)),
                     Telefono = reader.GetString(nameof(Inquilino.Telefono)),
                     Dni = reader.GetString(nameof(Inquilino.Dni)),
                     Email = reader.GetString(nameof(Inquilino.Email)),
