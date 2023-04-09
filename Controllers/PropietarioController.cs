9e9a4b87d17c71bf522af745835110cfbc6bd3a8
kaiserkey <kaiserkey2@gmail.com>
2023-04-08 21:52:17 -0300

    new changes

--- Controllers/PropietarioController.cs
+++ Controllers/PropietarioController.cs
@@ -47,7 +47,7 @@ namespace Inmobiliaria.Controllers
             try
             {
                 var res = RepoPropietario.CrearPropietario(con, propietario);
-
+                C
                 return RedirectToAction(nameof(Index));
             }
             catch
