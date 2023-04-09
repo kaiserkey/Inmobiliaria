ecd6e1a394862470c8c4601229fbd28c61a01b97
kaiserkey <kaiserkey2@gmail.com>
2023-04-08 23:22:50 -0300

    new changes

--- Controllers/InquilinoController.cs
+++ Controllers/InquilinoController.cs
@@ -70,7 +70,7 @@ namespace Inmobiliaria.Controllers
         {
             try
             {
-                RepoInquilino.EditInquilino(con, inquilino);
+                RepoInquilino.UpdateInquilino(con, inquilino);
 
                 return RedirectToAction(nameof(Index));
             }
