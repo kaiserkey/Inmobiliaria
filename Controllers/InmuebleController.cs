70809cf4a25e1499389b3db44045802b4211cde4
kaiserkey <kaiserkey2@gmail.com>
2023-04-09 23:34:59 -0300

    new changes

--- Controllers/InmuebleController.cs
+++ Controllers/InmuebleController.cs
@@ -63,7 +63,7 @@ namespace Inmobiliaria.Controllers
             return View(inmueble);
         }
 
-        
+        //obtener propietarios po
         public IActionResult BuscarPropietarios(string busqueda)
         {
             var propietarios = new List<Propietario>();
