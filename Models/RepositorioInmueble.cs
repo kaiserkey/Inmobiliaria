30d41d3a4adc62282116a8df8607146f1083a0a5
kaiserkey <kaiserkey2@gmail.com>
2023-04-10 11:43:50 -0300

    new changes

--- Models/RepositorioInmueble.cs
+++ Models/RepositorioInmueble.cs
@@ -100,7 +100,7 @@ public class RepositorioInmueble
         cmd.Parameters.AddWithValue("@IdPropietario", CreateInmueble.IdPropietario);
 
         res = Convert.ToInt32(cmd.ExecuteScalar());
-
+        CreateInmueble.
         mySqlDatabase.Dispose();
         
         return res;
