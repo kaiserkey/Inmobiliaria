f19c3d75a0c022ce4ed3ea2a5e98b3fb90ebcdf7
kaiserkey <kaiserkey2@gmail.com>
2023-04-10 19:49:49 -0300

    new changes

--- Models/Contrato.cs
+++ Models/Contrato.cs
@@ -5,5 +5,9 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 public class Contrato
 {
+    public int IdContrato { get; set; }
+    public int IdInmueble { get; set; }
+    public int IdInquilino { get; set; }
+    public DateTime FechaInicio { get; set; }
     
 }
