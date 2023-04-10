38a6fdd93774f5ea45268fd2f143dafc1761b303
kaiserkey <kaiserkey2@gmail.com>
2023-04-10 13:38:46 -0300

    new changes

--- Controllers/PropietarioController.cs
+++ Controllers/PropietarioController.cs
@@ -26,7 +26,7 @@ namespace Inmobiliaria.Controllers
             ViewBag.Id = TempData["Id"];
             if (TempData.ContainsKey("Mensaje")){
                 Console.WriteLine("Entro al tempdata");
-                ViewBag.Mensaje = "asdasdasdasdasda";
+                ViewBag.Mensaje = TempData["Mensaje"];
             }
             return View(listaPropietarios);
         }
