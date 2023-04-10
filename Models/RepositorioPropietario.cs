5134b35c6926078367b44f5f7809786da667c7e9
kaiserkey <kaiserkey2@gmail.com>
2023-04-10 12:09:43 -0300

    new changes

--- Models/RepositorioPropietario.cs
+++ Models/RepositorioPropietario.cs
@@ -65,7 +65,6 @@ public class RepositorioPropietario
                     return propietario;
                 }
             }
-            mySqlDatabase.Dispose();
         }
         return null;
     }
