45d72ac58c1546d92ed1e85d2e28476ee94e3e71
kaiserkey <kaiserkey2@gmail.com>
2023-04-09 23:18:27 -0300

    new changes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,28 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+$(document).ready(function () {
+    $("#PropietarioBusqueda").on("input", function () {
+        var busqueda = $("#PropietarioBusqueda").val();
+        $.getJSON("/Inmueble/BuscarPropietarios", { busqueda: busqueda }, function (resultados) {
+            var options = '<option value="">Selecciona una opción</option>';
+            $.each(resultados, function (index, resultado) {
+                options += '<option value="' + resultado.id + '">' + resultado.text + '</option>';
+            });
+            $("#selectPropietario").html(options);
+        });
+    });
+});
+$(document).ready(function () {
+    // Escondemos el select al cargar la página
+    $("#selectPropietario").hide();
 
-// Write your JavaScript code.
+    // Cuando hacemos clic en el input, mostramos el select
+    $("#PropietarioBusqueda").click(function () {
+        $("#selectPropietario").show();
+    });
+
+    // Cuando seleccionamos una opción del select, la agregamos al input
+    $("#selectPropietario").change(function () {
+        var selectedOption = $("#selectPropietario option:selected").text();
+        $("#PropietarioBusqueda").val(selectedOption);
+    });
+});
