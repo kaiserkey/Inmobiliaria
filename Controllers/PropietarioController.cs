5723e95429828bc13f7badb52bf9baf588629fc3
kaiserkey <kaiserkey2@gmail.com>
2023-04-10 10:48:50 -0300

    new changes

--- Controllers/PropietarioController.cs
+++ Controllers/PropietarioController.cs
@@ -23,6 +23,7 @@ namespace Inmobiliaria.Controllers
         public ActionResult Index()
         {
             var listaPropietarios = RepoPropietario.GetPropietarios(con);
+            ViewBag.Id = TempData["Id"];
             return View(listaPropietarios);
         }
 
