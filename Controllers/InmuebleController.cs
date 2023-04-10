cadd03877232af944c49a5f7b69108b0b1a7f511
kaiserkey <kaiserkey2@gmail.com>
2023-04-09 22:19:30 -0300

    new changes

--- Controllers/InmuebleController.cs
+++ Controllers/InmuebleController.cs
@@ -65,7 +65,7 @@ namespace Inmobiliaria.Controllers
 
         public IActionResult BuscarPropietarios(string busqueda)
         {
-            var propietarios = RepoPropietario.BuscarPropietario(con, )
+            var propietarios = RepoPropietario.BuscarPropietario(con, busqueda);
 
             var resultados = propietarios.Select(p => new
             {
