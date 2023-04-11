9e429deda5e85500203521b1e29c6d90ed1478a0
kaiserkey <kaiserkey2@gmail.com>
2023-04-10 23:19:17 -0300

    new changes

--- Controllers/ContratoController.cs
+++ Controllers/ContratoController.cs
@@ -30,7 +30,7 @@ namespace Inmobiliaria.Controllers
             }
             return View(listaContratos);
         }
-        // TODO: TERMINAR DELET
+        // TODO: TERMINAR DELETE Y DETAILS
         // GET: Contrato/Details/5
         public ActionResult Details(int id)
         {
