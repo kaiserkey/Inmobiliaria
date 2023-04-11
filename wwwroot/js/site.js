020ab9361b0550e8c624801960e5e09a2d74663c
kaiserkey <kaiserkey2@gmail.com>
2023-04-11 10:32:50 -0300

    new changes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -85,36 +85,6 @@ $(document).ready(function () {
     });
 });
 
-
-/* Buscar Propietarios */
-$(document).ready(function () {
-    $("#PropietarioBusqueda").on("input", function () {
-        var busqueda = $("#PropietarioBusqueda").val();
-        $.getJSON("/Inmueble/BuscarPropietarios", { busqueda: busqueda }, function (resultados) {
-            var options = '<option value="">Selecciona una opción</option>';
-            $.each(resultados, function (index, resultado) {
-                options += '<option value="' + resultado.id + '">' + resultado.text + '</option>';
-            });
-            $("#selectPropietario").html(options);
-        });
-    });
-});
-$(document).ready(function () {
-    // Escondemos el select al cargar la página
-    $("#selectPropietario").hide();
-
-    // Cuando hacemos clic en el input, mostramos el select
-    $("#PropietarioBusqueda").click(function () {
-        $("#selectPropietario").show();
-    });
-
-    // Cuando seleccionamos una opción del select, la agregamos al input
-    $("#selectPropietario").change(function () {
-        var selectedOption = $("#selectPropietario option:selected").text();
-        $("#PropietarioBusqueda").val(selectedOption);
-    });
-});
-
 /* Mostrar modales de mensajes */
 $(document).ready(function () {
     $('#modalOK').modal('show');
