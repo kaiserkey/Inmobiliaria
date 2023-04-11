ffbb86ac72fba9b1f83410f333969f679ac5b06a
kaiserkey <kaiserkey2@gmail.com>
2023-04-11 20:25:11 -0300

    new changes

--- Models/Pago.cs
+++ Models/Pago.cs
@@ -3,7 +3,7 @@ namespace Inmobiliaria.Models;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-public class Contrato
+public class Pago
 {
     
 }
