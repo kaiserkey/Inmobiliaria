70a0dde6837ba138d7f23593140a524febaec52e
kaiserkey <kaiserkey2@gmail.com>
2023-04-10 23:10:14 -0300

    new changes

--- Controllers/ContratoController.cs
+++ Controllers/ContratoController.cs
@@ -24,6 +24,7 @@ namespace Inmobiliaria.Controllers
         public ActionResult Index()
         {
             var listaContratos = RepoContrato.GetContratos(con);
+            
             return View(listaContratos);
         }
 
