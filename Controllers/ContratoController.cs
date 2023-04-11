117bdebefb1b447528b052923a4ae735c519273e
kaiserkey <kaiserkey2@gmail.com>
2023-04-11 17:26:16 -0300

    new changes

--- Controllers/ContratoController.cs
+++ Controllers/ContratoController.cs
@@ -87,7 +87,7 @@ namespace Inmobiliaria.Controllers
         public IActionResult BuscarInmuebles(string busqueda, string opcion)
         {
             var inmuebles = new List<Inmueble>();
-            inmuebles = RepoInmueble.BuscarInmueble(con, busqueda, buscarPor);
+            inmuebles = RepoInmueble.BuscarInmueble(con, busqueda, opcion);
 
             var resultados = inmuebles.Select(i => new
             {
