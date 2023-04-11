2194e0b6113e0c3f40543f51d4a4eeb73b79d49e
kaiserkey <kaiserkey2@gmail.com>
2023-04-11 20:00:50 -0300

    new changes

--- Models/RepositorioInmueble.cs
+++ Models/RepositorioInmueble.cs
@@ -152,7 +152,6 @@ public class RepositorioInmueble
         var inmuebles = new List<Inmueble>();
         using (var cmd = mySqlDatabase.Connection.CreateCommand() as MySqlCommand)
         {
-            Console.WriteLine(busqueda);
             cmd.CommandText = @"SELECT IdInmueble, Tipo, Coordenadas, Precio, Ambientes, Uso, Activo, IdPropietario 
                             FROM Inmueble 
                             WHERE " + buscarPor + " LIKE @busqueda AND Activo = 1";
