51562c1e47d3c278abb76dcf2025cd2086a0540b
kaiserkey <kaiserkey2@gmail.com>
2023-04-11 16:27:17 -0300

    new changes

--- Controllers/ContratoController.cs
+++ Controllers/ContratoController.cs
@@ -71,7 +71,7 @@ namespace Inmobiliaria.Controllers
 
             var resultados = inquilinos.Select(i => new
             {
-                id = i.IdInquilino,
+                idInquilino = i.IdInquilino,
                 text = i.Nombre + " " + i.Apellido
             });
 
