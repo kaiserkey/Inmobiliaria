bf209a3433baad4504edc0becd187b8f9faef196
kaiserkey <kaiserkey2@gmail.com>
2023-04-12 12:35:18 -0300

    new changes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -22,7 +22,7 @@ function buscarContratos() {
     });
 }
 
-function seleccionarCotrato(idContrato) {
+function seleccionarContrato(idContrato) {
     $('#IdContrato').val(idContrato);
     $('#modalBuscarContrato').modal('hide');
 }
