76e449fd6cd7e3d7c69f4977e2cc53c5c31d4851
kaiserkey <kaiserkey2@gmail.com>
2023-04-12 11:46:03 -0300

    new changes

--- Models/RepositorioContrato.cs
+++ Models/RepositorioContrato.cs
@@ -182,7 +182,12 @@ public class RepositorioContrato
         var contratos = new List<Contrato>();
         using (var cmd = mySqlDatabase.Connection.CreateCommand() as MySqlCommand)
         {
-            if (buscarPor == "Inquilino")
+            cmd.CommandText = @"SELECT c.IdContrato, c.IdInquilino, c.IdInmueble, c.FechaInicio, c.FechaFin,
+                            i.Nombre, i.Apellido, i.Dni
+                            FROM Contrato c
+                            INNER JOIN Inquilino i ON c.IdInquilino = i.IdInquilino
+                            WHERE CONCAT(i.Nombre, ' ', i.Apellido) LIKE @busqueda";
+            /* if (buscarPor == "Inquilino")
             {
                 cmd.CommandText = @"SELECT c.IdContrato, c.IdInquilino, c.IdInmueble, c.FechaInicio, c.FechaFin,
                             i.Nombre, i.Apellido, i.Dni
@@ -197,7 +202,7 @@ public class RepositorioContrato
                             FROM Contrato c
                             INNER JOIN Inquilino i ON c.IdInquilino = i.IdInquilino 
                             WHERE " + buscarPor + " LIKE @busqueda";
-            }
+            } */
 
             cmd.Parameters.AddWithValue("@busqueda", "%" + busqueda + "%");
             using (var reader = cmd.ExecuteReader())
