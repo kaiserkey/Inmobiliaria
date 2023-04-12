d1209241a6fbc6930119ae128b80f2f08627a735
kaiserkey <kaiserkey2@gmail.com>
2023-04-12 11:28:09 -0300

    new changes

--- Controllers/PagoController.cs
+++ Controllers/PagoController.cs
@@ -62,7 +62,7 @@ namespace Inmobiliaria.Controllers
             var contrato = new List<Contrato>();
             
             contrato = RepoContrato.BuscarContrato(con, busqueda, buscarPor);
-            Console.WriteLine(contrato.Nombre);
+            Console.WriteLine(contrato.Inquilino);
             var resultados = contrato.Select(c => new
             {
                 idContrato = c.IdContrato,
