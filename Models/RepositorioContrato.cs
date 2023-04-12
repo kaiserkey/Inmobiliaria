cf0d7010424be63ee938d107833ac2d6975d6e2d
kaiserkey <kaiserkey2@gmail.com>
2023-04-12 10:54:24 -0300

    new changes

--- Models/RepositorioContrato.cs
+++ Models/RepositorioContrato.cs
@@ -186,7 +186,7 @@ public class RepositorioContrato
             if(buscarPor == "Inquilino")
             {
                 cmd.CommandText = @"SELECT c.IdContrato, c.IdInquilino, c.IdInmueble, c.FechaInicio, c.FechaFin
-                                    
+                                    i.Nombre, i.Apellido
                                     FROM Contrato c
                                     INNER JOIN Inquilino i ON c.IdInquilino = i.IdInquilino
                                     WHERE CONCAT(i.Nombre, ' ', i.Apellido) LIKE @busqueda";
