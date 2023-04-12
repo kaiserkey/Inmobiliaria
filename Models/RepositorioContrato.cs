060b23d65c83724173908b7ea789344818d5846a
kaiserkey <kaiserkey2@gmail.com>
2023-04-12 10:46:09 -0300

    new changes

--- Models/RepositorioContrato.cs
+++ Models/RepositorioContrato.cs
@@ -209,7 +209,7 @@ public class RepositorioContrato
                         IdInmueble = reader.GetInt32(nameof(Contrato.IdInmueble)),
                         FechaInicio = reader.GetDateTime(nameof(Contrato.FechaInicio)),
                         FechaFin = reader.GetDateTime(nameof(Contrato.FechaFin)),
-                        Inquilino.
+                        Inquilino.Nombre = reader.GetString(nameof(Inquilino.Nombre)),
                     };
                     contratos.Add(contrato);
                 }
