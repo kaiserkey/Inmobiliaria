adc808c92ca350d598e3c669dfa87f49e3b78e9c
kaiserkey <kaiserkey2@gmail.com>
2023-04-12 12:02:43 -0300

    new changes

--- Models/RepositorioContrato.cs
+++ Models/RepositorioContrato.cs
@@ -184,7 +184,7 @@ public class RepositorioContrato
         {
             var query = "";
             /* buscarPor = "Inquilino"; */
-            Console.WriteLine(buscarPor.Equals("Inquilino"));
+            Console.WriteLine(buscarPor.Equals("Inquilino", StringComparison.OrdinalIgnoreCase));
             if (buscarPor == "Inquilino")
             {
                 query = @"SELECT c.IdContrato, c.IdInquilino, c.IdInmueble, c.FechaInicio, c.FechaFin,
