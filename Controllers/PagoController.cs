7003c7d2e823eeed90b1217d5bf15b0d63d4b0ad
kaiserkey <kaiserkey2@gmail.com>
2023-04-12 11:10:02 -0300

    new changes

--- Controllers/PagoController.cs
+++ Controllers/PagoController.cs
@@ -55,7 +55,7 @@ namespace Inmobiliaria.Controllers
             }
         }
 
-        /* buscar contratos por jquer */
+        /* buscar contratos por jquery */
         public IActionResult BuscarContratos(string busqueda, string buscarPor)
         {
             var contrato = new List<Contrato>();
