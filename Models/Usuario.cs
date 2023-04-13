a8774ac5c606c093b5344173a1ba199a4d867ce5
kaiserkey <kaiserkey2@gmail.com>
2023-04-13 20:36:57 -0300

    new changes

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -7,36 +7,36 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace Inmobiliaria.Models
 {
     [Key]
-		[Display(Name = "Código")]
-		public int Id { get; set; }
-		[Required]
-		public string Nombre { get; set; }
-		[Required]
-		public string Apellido { get; set; }
-		[Required, EmailAddress]
-		public string Email { get; set; }
-		[Required, DataType(DataType.Password)]
-		public string Clave { get; set; }
-		public string Avatar { get; set; }
-		[NotMapped]//Para EF
-		public IFormFile AvatarFile { get; set; }
-		//[NotMapped]//Para EF
-		//public byte[] AvatarFileContent { get; set; }
-		//[NotMapped]//Para EF
-		//public string AvatarFileName { get; set; }
-		public int Rol { get; set; }
-		[NotMapped]//Para EF
-		public string RolNombre => Rol > 0 ? ((enRoles)Rol).ToString() : "";
+    [Display(Name = "Código")]
+    public int Id { get; set; }
+    [Required]
+    public string Nombre { get; set; }
+    [Required]
+    public string Apellido { get; set; }
+    [Required, EmailAddress]
+    public string Email { get; set; }
+    [Required, DataType(DataType.Password)]
+    public string Clave { get; set; }
+    public string Avatar { get; set; }
+    [NotMapped]//Para EF
+    public IFormFile AvatarFile { get; set; }
+    //[NotMapped]//Para EF
+    //public byte[] AvatarFileContent { get; set; }
+    //[NotMapped]//Para EF
+    //public string AvatarFileName { get; set; }
+    public int Rol { get; set; }
+    [NotMapped]//Para EF
+    public string RolNombre => Rol > 0 ? ((enRoles)Rol).ToString() : "";
 
-		public static IDictionary<int, string> ObtenerRoles()
-		{
-			SortedDictionary<int, string> roles = new SortedDictionary<int, string>();
-			Type tipoEnumRol = typeof(enRoles);
-			foreach (var valor in Enum.GetValues(tipoEnumRol))
-			{
-				roles.Add((int)valor, Enum.GetName(tipoEnumRol, valor));
-			}
-			return roles;
-		}
-	}
+    public static IDictionary<int, string> ObtenerRoles()
+    {
+        SortedDictionary<int, string> roles = new SortedDictionary<int, string>();
+        Type tipoEnumRol = typeof(enRoles);
+        foreach (var valor in Enum.GetValues(tipoEnumRol))
+        {
+            roles.Add((int)valor, Enum.GetName(tipoEnumRol, valor));
+        }
+        return roles;
+    }
+
 }
