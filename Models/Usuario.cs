2c50d088e46ae7b0b55ef1d1c1a5be1cd009f1c3
kaiserkey <kaiserkey2@gmail.com>
2023-04-13 20:36:10 -0300

    new changes

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,7 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+namespace Inmobiliaria.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 
 namespace Inmobiliaria.Models
