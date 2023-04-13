2e0939fdbe1d5c86a479b7997ca1a8323369a2b4
kaiserkey <kaiserkey2@gmail.com>
2023-04-13 20:37:06 -0300

    new changes

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -28,15 +28,6 @@ namespace Inmobiliaria.Models
     [NotMapped]//Para EF
     public string RolNombre => Rol > 0 ? ((enRoles)Rol).ToString() : "";
 
-    public static IDictionary<int, string> ObtenerRoles()
-    {
-        SortedDictionary<int, string> roles = new SortedDictionary<int, string>();
-        Type tipoEnumRol = typeof(enRoles);
-        foreach (var valor in Enum.GetValues(tipoEnumRol))
-        {
-            roles.Add((int)valor, Enum.GetName(tipoEnumRol, valor));
-        }
-        return roles;
-    }
+    
 
 }
