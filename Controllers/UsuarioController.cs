8db6f2a687f1e4694dfd05f318681315ebe293a7
kaiserkey <kaiserkey2@gmail.com>
2023-04-16 15:46:10 -0300

    new changes

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -111,7 +111,7 @@ namespace Inmobiliaria.Controllers
         }
 
         // GET: Usuario/Create
-        [Authorize(Policy = "Administrador")]
+        //[Authorize(Policy = "Administrador")]
         public ActionResult Create()
         {
             ViewBag.Roles = Usuario.ObtenerRoles();
@@ -121,7 +121,7 @@ namespace Inmobiliaria.Controllers
         // POST: Usuario/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        [Authorize(Policy = "Administrador")]
+        //[Authorize(Policy = "Administrador")]
         public ActionResult Create(Usuario usuario)
         {
             try
