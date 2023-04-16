7ebd004c47b4fc4460e85a269faf8b094e0ef84c
kaiserkey <kaiserkey2@gmail.com>
2023-04-15 22:14:14 -0300

    new changes

--- Models/RepositorioUsuario.cs
+++ Models/RepositorioUsuario.cs
@@ -1,12 +1,10 @@
 using System;
+using MySql.Data.MySqlClient;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace Inmobiliaria.Models
-{
+namespace Inmobiliaria.Models;
+
     public class RepositorioUsuario
     {
         
     }
-}
