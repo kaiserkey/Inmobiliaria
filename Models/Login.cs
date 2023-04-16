9ebc49a4b53ddbe79e887fe67f35f71aad3c1e58
kaiserkey <kaiserkey2@gmail.com>
2023-04-16 13:27:31 -0300

    new changes

--- Models/Login.cs
+++ Models/Login.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Inmobiliaria.Models
-{
-    public class Login
-    {
-        
-    }
+namespace Inmobiliaria.Models;
+public class Login{
+    public string Usuario { get ; set ; }
+    public string? Clave { get ; set ; }
 }
