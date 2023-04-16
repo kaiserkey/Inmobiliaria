6aa63d6b483c7fb252ab846d211be7ef7a155bf9
kaiserkey <kaiserkey2@gmail.com>
2023-04-15 21:52:20 -0300

    new changes

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -11,7 +11,7 @@ public class Usuario
     public int Id { get; set; }
     [Required]
     public string Nombre { get; set; }
-    
+    [Required]
     public string Apellido { get; set; }
     
     public string Email { get; set; }
