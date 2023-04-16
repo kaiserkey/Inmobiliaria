bea6d97f3d4f662491f49a6e86f485655c432909
kaiserkey <kaiserkey2@gmail.com>
2023-04-16 13:32:47 -0300

    new changes

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -45,7 +45,6 @@ namespace Inmobiliaria.Controllers
         // POST: Usuario/Login/
 		[HttpPost]
 		[AllowAnonymous]
-		[ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(Login login){
             try
             {
