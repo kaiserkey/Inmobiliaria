2690b55f36f1ea938e1adceff354e3c923784402
kaiserkey <kaiserkey2@gmail.com>
2023-04-16 14:33:20 -0300

    new changes

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -136,7 +136,7 @@ namespace Inmobiliaria.Controllers
                 usuario.Clave = hashed;
                 //usuario.Rol = User.IsInRole("Administrador") ? usuario.Rol : (int)enRoles.Empleado ;
                 var res = RepoUsuario.CreateUsuario(con, usuario);
-                if (usuario.AvatarFile != null && usuario.Id > 0)
+                if (usuario.AvatarFile != null && usuario.IdUsuario > 0)
                 {
                     string wwwPath = environment.WebRootPath;
                     string path = Path.Combine(wwwPath, "Uploads");
