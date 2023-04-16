c1e45ef8fb3112e761c7f61666d7fbd07849dd13
kaiserkey <kaiserkey2@gmail.com>
2023-04-16 15:27:28 -0300

    new changes

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -254,7 +254,7 @@ namespace Inmobiliaria.Controllers
         {
             try
             {
-                
+                var res = RepoUsuario.DeleteUsuario(con, id);
 
                 return RedirectToAction(nameof(Index));
             }
