976df3e7492c91ca6d9cb73a736c099a03f95298
kaiserkey <kaiserkey2@gmail.com>
2023-04-16 14:28:23 -0300

    new changes

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -12,7 +12,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication;
 
 namespace Inmobiliaria.Controllers
-{   
+{
     [Authorize]
     public class UsuarioController : Controller
     {
@@ -20,7 +20,7 @@ namespace Inmobiliaria.Controllers
         private readonly RepositorioUsuario RepoUsuario;
         private readonly IConfiguration configuration;
         private readonly IWebHostEnvironment environment;
-        
+
         public UsuarioController(IConfiguration configuration, IWebHostEnvironment environment)
         {
             con = new MySqlDatabase();
@@ -44,57 +44,58 @@ namespace Inmobiliaria.Controllers
         }
 
         // POST: Usuario/Login/
-		[HttpPost]
-		[AllowAnonymous]
-		[ValidateAntiForgeryToken]
-        public async Task<IActionResult> Login(Login login){
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(Login login)
+        {
             try
             {
                 if (ModelState.IsValid)
-				{
-					string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
-						password: login.Clave,
-						salt: System.Text.Encoding.ASCII.GetBytes(configuration["Salt"]),
-						prf: KeyDerivationPrf.HMACSHA1,
-						iterationCount: 1000,
-						numBytesRequested: 256 / 8));
+                {
+                    string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                        password: login.Clave,
+                        salt: System.Text.Encoding.ASCII.GetBytes(configuration["Salt"]),
+                        prf: KeyDerivationPrf.HMACSHA1,
+                        iterationCount: 1000,
+                        numBytesRequested: 256 / 8));
 
-					var Usuario = RepoUsuario.ObtenerPorEmail(con, login.Usuario);
-					if (Usuario == null || Usuario.Clave != hashed)
-					{
-                        ModelState.AddModelError("","Usuario o contraseña incorrecto");
+                    var Usuario = RepoUsuario.ObtenerPorEmail(con, login.Usuario);
+                    if (Usuario == null || Usuario.Clave != hashed)
+                    {
+                        ModelState.AddModelError("", "Usuario o contraseña incorrecto");
                         return View();
                     }
 
-					var claims = new List<Claim>
-					{
-						new Claim(ClaimTypes.Name, Usuario.Email),
-						new Claim("FullName", Usuario.Nombre + " " + Usuario.Apellido),
-						new Claim(ClaimTypes.Role, Usuario.RolNombre),
-					};
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, Usuario.Email),
+                        new Claim("FullName", Usuario.Nombre + " " + Usuario.Apellido),
+                        new Claim(ClaimTypes.Role, Usuario.RolNombre),
+                    };
 
-					var claimsIdentity = new ClaimsIdentity(
-							claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    var claimsIdentity = new ClaimsIdentity(
+                            claims, CookieAuthenticationDefaults.AuthenticationScheme);
 
-					await HttpContext.SignInAsync(
-							CookieAuthenticationDefaults.AuthenticationScheme,
-							new ClaimsPrincipal(claimsIdentity));
+                    await HttpContext.SignInAsync(
+                            CookieAuthenticationDefaults.AuthenticationScheme,
+                            new ClaimsPrincipal(claimsIdentity));
 
-					
-					return Redirect("/Home");
-				}
 
-				return View();
+                    return Redirect("/Home");
+                }
+
+                return View();
             }
             catch (Exception ex)
             {
-                ModelState.AddModelError("",ex.Message);
+                ModelState.AddModelError("", ex.Message);
                 return View();
             }
         }
 
         // GET: Usuario
-        [Authorize(Policy ="Administrador")]
+        [Authorize(Policy = "Administrador")]
         public ActionResult Index()
         {
             var usuarios = RepoUsuario.GetUsuarios(con);
@@ -102,7 +103,7 @@ namespace Inmobiliaria.Controllers
         }
 
         // GET: Usuario/Details/5
-        [Authorize(Policy ="Administrador")]
+        [Authorize(Policy = "Administrador")]
         public ActionResult Details(int id)
         {
             Usuario usuario = RepoUsuario.GetUsuario(con, id);
@@ -110,7 +111,7 @@ namespace Inmobiliaria.Controllers
         }
 
         // GET: Usuario/Create
-        [Authorize(Policy ="Administrador")]
+        [Authorize(Policy = "Administrador")]
         public ActionResult Create()
         {
             ViewBag.Roles = Usuario.ObtenerRoles();
@@ -120,12 +121,38 @@ namespace Inmobiliaria.Controllers
         // POST: Usuario/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        [Authorize(Policy ="Administrador")]
+        [Authorize(Policy = "Administrador")]
         public ActionResult Create(Usuario usuario)
         {
             try
             {
-                
+                string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                        password: usuario.Clave,
+                        salt: System.Text.Encoding.ASCII.GetBytes(configuration["Salt"]),
+                        prf: KeyDerivationPrf.HMACSHA1,
+                        iterationCount: 1000,
+                        numBytesRequested: 256 / 8
+                    ));
+                usuario.Clave = hashed;
+                //usuario.Rol = User.IsInRole("Administrador") ? usuario.Rol : (int)enRoles.Empleado ;
+                var res = Repo.CrearUsuario(usuario);
+                if (usuario.AvatarFile != null && usuario.Id > 0)
+                {
+                    string wwwPath = environment.WebRootPath;
+                    string path = Path.Combine(wwwPath, "Uploads");
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+                    string fileName = "avatar_" + usuario.Id + Path.GetExtension(usuario.AvatarFile.FileName);
+                    string pathCompleto = Path.Combine(path, fileName);
+                    usuario.Avatar = Path.Combine("/Uploads", fileName);
+                    using (FileStream stream = new FileStream(pathCompleto, FileMode.Create))
+                    {
+                        usuario.AvatarFile.CopyTo(stream);
+                    }
+                    Repo.EditarUsuario(usuario);
+                }
 
                 return RedirectToAction(nameof(Index));
             }
