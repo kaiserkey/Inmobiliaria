ad69fab945dd7a0d15a2ef1c091bc5c70308dacc
kaiserkey <kaiserkey2@gmail.com>
2023-04-15 22:15:37 -0300

    new changes

--- Models/RepositorioUsuario.cs
+++ Models/RepositorioUsuario.cs
@@ -40,5 +40,31 @@ public class RepositorioUsuario
         return usuarios;
     }
 
-    
+    public Usuario GetUsuario(MySqlDatabase mySqlDatabase, int id)
+    {
+        Usuario? usuario = null;
+        using (var cmd = mySqlDatabase.Connection.CreateCommand() as MySqlCommand)
+        {
+            cmd.CommandText = @"SELECT IdUsuario, Nombre, Apellido, Direccion, Telefono, Dni, Email, Rol FROM Usuario WHERE IdUsuario = @id";
+            cmd.Parameters.AddWithValue("@id", id);
+            using (var reader = cmd.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    usuario = new Usuario
+                    {
+                        IdUsuario = reader.GetInt32(nameof(Usuario.IdUsuario)),
+                        Nombre = reader.GetString(nameof(Usuario.Nombre)),
+                        Apellido = reader.GetString(nameof(Usuario.Apellido)),
+                        Direccion = reader.GetString(nameof(Usuario.Direccion)),
+                        Telefono = reader.GetString(nameof(Usuario.Telefono)),
+                        Dni = reader.GetString(nameof(Usuario.Dni)),
+                        Email = reader.GetString(nameof(Usuario.Email)),
+                        Rol = reader.GetString(nameof(Usuario.Rol))
+                    };
+                }
+            }
+        }
+        return usuario;
+    }
 }
