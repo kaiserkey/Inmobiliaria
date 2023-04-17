43a8cafc7a53fd478d9869884a519635fde9bcba
kaiserkey <kaiserkey2@gmail.com>
2023-04-17 11:08:37 -0300

    new changes

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,16 +1,16 @@
-using Internal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Inmobiliaria.Repositorios;
 using Microsoft.AspNetCore.Authorization;
-using Inmobiliaria.Models;
 using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication;
+using Inmobiliaria.Models;
 
 namespace Inmobiliaria.Controllers
 {
