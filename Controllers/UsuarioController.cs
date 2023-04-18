a68743150570c0486c480a7df99407f72158c8d1
kaiserkey <kaiserkey2@gmail.com>
2023-04-18 20:45:02 -0300

    new changes

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -259,7 +259,7 @@ namespace Inmobiliaria.Controllers
         // POST: Usuario/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        [Authorize(Policy = "Administrador")]
+        [Authorize(Policy = "E")]
         public ActionResult EditarPerfil(int id, Usuario usuarioEdit)
         {
             var usuario = RepoUsuario.GetUsuario(con, id);
