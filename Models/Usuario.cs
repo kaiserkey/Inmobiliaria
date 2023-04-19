1f3dee67b9f7ae8dc5f5a0da62e74561c6b5b935
kaiserkey <kaiserkey2@gmail.com>
2023-04-19 20:17:28 -0300

    new changes

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -22,6 +22,8 @@ public class Usuario
     [Required, DataType(DataType.Password)]
     public string Clave { get; set; }
 
+    public string 
+
     public string Avatar { get; set; }
     [NotMapped]//Para EF
     public IFormFile AvatarFile { get; set; }
