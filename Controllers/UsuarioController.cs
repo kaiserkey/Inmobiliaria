f952125f67216d11145c8163696e3a0b530fae16
kaiserkey <kaiserkey2@gmail.com>
2023-04-19 21:48:54 -0300

    new changes

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -235,7 +235,7 @@ namespace Inmobiliaria.Controllers
             Console.WriteLine("Editar perfil");
             var usuario = RepoUsuario.ObtenerPorEmail(con, User.Identity.Name);
             id = usuario.IdUsuario;
-            usuarioEdit.Rol = User.Identity;
+            usuarioEdit.Rol = User.Identity.R;
             try
             {
                 var usuarioActual = RepoUsuario.ObtenerPorEmail(con, User.Identity.Name);
