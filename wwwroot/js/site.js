e4cba06838d9267a83454e5ae3eb3a719fb55d9d
kaiserkey <kaiserkey2@gmail.com>
2023-04-20 22:19:52 -0300

    new changes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -185,7 +185,7 @@ $(document).ready(function () {
 
 //vista previa de imagen usuario
 //agegarle la funcionalidad para que la imagen seleccionada por defecto sea 
-//
+//la /img/default.webp
 function mostrarImagenSeleccionada(event) {
     var imagen = event.target.files[0];
     var reader = new FileReader();
