e0c46bf3a6e73751bf36eb2b905ade119f698bb0
kaiserkey <kaiserkey2@gmail.com>
2023-04-21 13:30:21 -0300

    new changes

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -137,7 +137,7 @@ namespace Inmobiliaria.Controllers
                         numBytesRequested: 256 / 8
                     ));
                 usuario.Clave = hashed;
-                Console.WriteLine();
+                Console.WriteLine(usuario.);
                 var res = RepoUsuario.CreateUsuario(con, usuario);
                 
                 if (usuario.AvatarFile != null && usuario.IdUsuario > 0)
