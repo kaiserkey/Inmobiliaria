bf76bb976bb507fcafb7e9d42c3e2b1e95289a75
kaiserkey <kaiserkey2@gmail.com>
2023-04-23 19:58:40 -0300

    new changes

--- Models/RepositorioInmueble.cs
+++ Models/RepositorioInmueble.cs
@@ -197,7 +197,7 @@ public class RepositorioInmueble
                                 FROM Inmueble i
                                 JOIN Propietario p ON i.IdPropietario = p.IdPropietario
                                 WHERE (p.Nombre LIKE @busqueda OR p.Dni LIKE @busqueda) LIMIT 10";
-                Console.WriteLine("Propietario");
+                Console.WriteLine(busqueda);
                 cmd.Parameters.AddWithValue("@busqueda", "%" + busqueda + "%");
             }
             
