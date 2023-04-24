81d5a7e92a3066a94e0cd90a96da7b4b23e72fb9
kaiserkey <kaiserkey2@gmail.com>
2023-04-24 15:50:33 -0300

    new changes

--- Models/RepositorioContrato.cs
+++ Models/RepositorioContrato.cs
@@ -230,6 +230,9 @@ public class RepositorioContrato
 
     public List<Contrato> BuscarContratosPorFecha(MySqlDatabase mySqlDatabase, string fechaDesde, string fechaHasta)
     {
+         // Convertir las fechas al formato de la base de datos
+        var fechaDesdeDB = DateTime.ParseExact(fechaDesde, "dd/MM/yyyy", CultureInfo.InvariantCulture).ToString("yyyy-MM-dd HH:mm:ss");
+        var fechaHastaDB = DateTime.ParseExact(fechaHasta, "dd/MM/yyyy", CultureInfo.InvariantCulture).ToString("yyyy-MM-dd HH:mm:ss");
         var contratos = new List<Contrato>();
         using (var cmd = mySqlDatabase.Connection.CreateCommand() as MySqlCommand)
         {
