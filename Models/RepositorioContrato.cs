80b85623959002cb2e24df34e84b8fd9d333087c
kaiserkey <kaiserkey2@gmail.com>
2023-04-24 15:57:10 -0300

    new changes

--- Models/RepositorioContrato.cs
+++ Models/RepositorioContrato.cs
@@ -233,6 +233,7 @@ public class RepositorioContrato
          // Convertir las fechas al formato de la base de datos
         var fechaDesdeDB = DateTime.ParseExact(fechaDesde, "dd/MM/yyyy", CultureInfo.InvariantCulture).ToString("yyyy-MM-dd HH:mm:ss");
         var fechaHastaDB = DateTime.ParseExact(fechaHasta, "dd/MM/yyyy", CultureInfo.InvariantCulture).ToString("yyyy-MM-dd HH:mm:ss");
+        
         var contratos = new List<Contrato>();
         using (var cmd = mySqlDatabase.Connection.CreateCommand() as MySqlCommand)
         {
