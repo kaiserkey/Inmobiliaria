aa1adcf8b3f6ccda548ee10e30e27f2ee49f05fa
kaiserkey <kaiserkey2@gmail.com>
2023-04-24 15:38:27 -0300

    new changes

--- Models/RepositorioContrato.cs
+++ Models/RepositorioContrato.cs
@@ -231,7 +231,6 @@ public class RepositorioContrato
     public List<Contrato> BuscarContratosPorFecha(MySqlDatabase mySqlDatabase, string fechaDesde, string fechaHasta, string buscarPor)
     {
 
-
         var contratos = new List<Contrato>();
         using (var cmd = mySqlDatabase.Connection.CreateCommand() as MySqlCommand)
         {
