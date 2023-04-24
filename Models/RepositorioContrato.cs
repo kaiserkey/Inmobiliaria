793a7f4872fa96c3442e17e4d9f149f67e1405d8
kaiserkey <kaiserkey2@gmail.com>
2023-04-24 15:27:16 -0300

    new changes

--- Models/RepositorioContrato.cs
+++ Models/RepositorioContrato.cs
@@ -235,7 +235,7 @@ public class RepositorioContrato
         using (var cmd = mySqlDatabase.Connection.CreateCommand() as MySqlCommand)
         {
 
-            var query = @"SELECT c.IdContrato, c.IdInquilino, c.IdInmueble, c.FechaInicio, c.FechaFin,
+            cmd.CommandText = @"SELECT c.IdContrato, c.IdInquilino, c.IdInmueble, c.FechaInicio, c.FechaFin,
                 i.Nombre, i.Apellido, i.Dni
                 FROM Contrato c
                 INNER JOIN Inquilino i ON c.IdInquilino = i.IdInquilino
