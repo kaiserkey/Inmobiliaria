27ac63088ae406d4f105721c281dda2753eda8fe
kaiserkey <kaiserkey2@gmail.com>
2023-04-24 14:03:42 -0300

    new changes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
         $(".optionInputContrato").empty();
         if (selectedOption == "Fecha") {
             $(".optionInputContrato").append('<label for="fechaDesde" class="input-group-text">Desde:</label>');
-            $(".optionInputContrato").append('<input type="date" class="form-control mb-3" id="fechaDesde">');
+            $(".optionInputContrato").append('<input type="date" class="form-control" id="fechaDesde">');
             $(".optionInputContrato").append('<label for="fechaHasta" class="input-group-text">Hasta:</label>');
             $(".optionInputContrato").append('<input type="date" class="form-control" id="fechaHasta">');
         } else {
@@ -44,7 +44,7 @@ $(document).ready(function () {
     });
     // por defecto, agregar los inputs de fecha
     $(".optionInputContrato").append('<label for="fechaDesde" class="input-group-text">Desde:</label>');
-    $(".optionInputContrato").append('<input type="date" class="form-control mb-3" id="fechaDesde">');
+    $(".optionInputContrato").append('<input type="date" class="form-control" id="fechaDesde">');
     $(".optionInputContrato").append('<label for="fechaHasta" class="input-group-text">Hasta:</label>');
     $(".optionInputContrato").append('<input type="date" class="form-control" id="fechaHasta">');
 });
