23d3131bae009033abb2423fcbf5b375011e66b9
kaiserkey <kaiserkey2@gmail.com>
2023-04-24 14:15:38 -0300

    new changes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -40,7 +40,7 @@ $(document).ready(function () {
             $(".optionInputContrato").append('<input type="date" class="form-control" id="fechaHasta">');
         } else {
             $(".optionInputContrato").append('<label for="fechaDesde" class="input-group-text">Desde:</label>');
-            $(".optionInputContrato").append('<input type="codi" class="form-control" id="inputNumerico">');
+            $(".optionInputContrato").append('<input type="number" class="form-control" id="inputNumerico">');
         }
     });
     // por defecto, agregar los inputs de fecha
