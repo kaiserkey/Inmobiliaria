aea36f2ad4114d22be9b6a039eccfed82e7ba04a
kaiserkey <kaiserkey2@gmail.com>
2023-04-25 11:10:46 -0300

    new changes

--- Models/RepositorioContrato.cs
+++ Models/RepositorioContrato.cs
@@ -119,7 +119,8 @@ public class RepositorioContrato
     }
 
     public int CancelarContrato(MySqlDatabase mySqlDatabase, int idContrato, DateTime fechaFin, double importe){
-        //obtener el importe segun el pago de los me
+        //obtener el importe segun el pago de los meses de alquiler
+        
         // Obtener el contrato correspondiente a partir de su ID
         var contrato = GetContrato(mySqlDatabase, idContrato);
         // Calcular la duración original del contrato en meses
