02279e06d75e8319fa0844dc4ce6ae82edfc78b9
kaiserkey <kaiserkey2@gmail.com>
2023-04-25 11:23:59 -0300

    new changes

--- Controllers/ContratoController.cs
+++ Controllers/ContratoController.cs
@@ -176,11 +176,13 @@ namespace Inmobiliaria.Controllers
                 return RedirectToAction(nameof(Index));
             }
             var res = RepoContrato.CancelarContrato(con, id, DateTime.Now, pago.IdContrato);
+            
             if(res > 0){
                 TempData["Mensaje"] = "Se ha cancelado con exito el contrato ID:" + id;
             }else {
                 TempData["Mensaje"] = "No se pudo cancelar el contrato ID:" + id;
             }
+            
             return View(nameof(Index));
         }
 
