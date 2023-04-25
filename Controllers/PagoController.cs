09413c45f035a61e78c731d0220a9cd6511f631b
kaiserkey <kaiserkey2@gmail.com>
2023-04-24 22:47:08 -0300

    new changes

--- Controllers/PagoController.cs
+++ Controllers/PagoController.cs
@@ -95,6 +95,28 @@ namespace Inmobiliaria.Controllers
             return Json(resultados);
         }
 
+        /* buscar pagos por jquery */
+        public IActionResult BuscarContratos(string busqueda, string opcion)
+        {
+            var contrato = new List<Contrato>();
+            
+            contrato = RepoContrato.BuscarContrato(con, busqueda, opcion);
+            
+            var resultados = contrato.Select(c => new
+            {
+                idContrato = c.IdContrato,
+                idInmueble = c.IdInmueble,
+                idInquilino = c.IdInquilino,
+                nombre = c.Inquilino.Nombre,
+                apellido = c.Inquilino.Apellido,
+                dni = c.Inquilino.Dni,
+                fechaFin = c.FechaFin,
+                fechaInicio = c.FechaInicio,
+            });
+
+            return Json(resultados);
+        }
+
         // GET: Pago/Edit/5
         [Authorize]
         public ActionResult Edit(int id)
