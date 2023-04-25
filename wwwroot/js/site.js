031d59d9aedd79b93de93d8071c64e084622ab78
kaiserkey <kaiserkey2@gmail.com>
2023-04-25 08:07:44 -0300

    new changes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -103,6 +103,7 @@ function abrirModalBuscarInmueble() {
 function buscarInmuebles() {
     var busqueda = $('#txtBuscar').val();
     var opcion = $('#buscarInmueblePor').val();
+    if <option value=""></option>
     $.getJSON('/Inmueble/BuscarInmuebles', { busqueda: busqueda, opcion: opcion }, function (resultados) {
         $('#tblInmuebles').empty();
         $.each(resultados, function (index, resultado) {
