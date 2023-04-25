2ff32a1b402e071eb1187e93bbdee46ede175d0c
kaiserkey <kaiserkey2@gmail.com>
2023-04-24 21:12:41 -0300

    new changes

--- Models/RepositorioContrato.cs
+++ Models/RepositorioContrato.cs
@@ -280,7 +280,7 @@ public class RepositorioContrato
             i.Nombre, i.Apellido, i.Dni
             FROM Contrato c
             INNER JOIN Inquilino i ON c.IdInquilino = i.IdInquilino
-            WHERE c.FechaInicio <= @fechaHasta AND c.FechaFin >= @fechaDesde
+            WHERE c.
             LIMIT 10";
 
             cmd.Parameters.AddWithValue("@fechaDesde", fechaDesde);
