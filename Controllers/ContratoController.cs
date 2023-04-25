af56d15c97121d5b0e92d1495349f789514aed9d
kaiserkey <kaiserkey2@gmail.com>
2023-04-25 08:48:44 -0300

    new changes

--- Controllers/ContratoController.cs
+++ Controllers/ContratoController.cs
@@ -177,7 +177,7 @@ namespace Inmobiliaria.Controllers
                 if(res > 0){
                     TempData["Mensaje"] = "La entidad se actualizo correctamente ID:" + id;
                 }else{
-                    TempData["Mensaje"] = "Existe superposicion de fechas con otra " + id;
+                    TempData["Mensaje"] = "Existe superposicion de fechas con otro contrato";
                 }
                 
                 return RedirectToAction(nameof(Index));
