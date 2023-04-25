fe6d347c6bb2fc5b026f26529cda292e4cfdcd16
kaiserkey <kaiserkey2@gmail.com>
2023-04-25 07:55:47 -0300

    new changes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -131,7 +131,7 @@ $(document).ready(function () {
             $(".optionInputInmueble").append('<input type="date" class="form-control" id="fechaHasta">');
         } else if (selectedOption == "Propietario"){
             $(".optionInputInmueble").append('<label for="Codigo" class="input-group-text">Codigo: </label>');
-            $(".optionInputInmueble").append('<input type="number" class="form-control" id="Codigo">');
+            $(".optionInputInmueble").append('<input type="number" id="txtBuscar" class="form-control" placeholder="Codigo Contrato" />');
         }
     });
 });
