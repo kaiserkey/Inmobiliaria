d24fa3eb22f6acb7d4a36f700fd48dbe61948602
kaiserkey <kaiserkey2@gmail.com>
2023-04-25 08:15:52 -0300

    new changes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -122,6 +122,7 @@ function buscarInmuebles() {
     if(option == "InmuebleNotIn"){
         var fechaInicio = $.datepicker.formatDate("yy-mm-dd", new Date($('#fechaInicio').val())) + " 00:00:00";
         var fechaFin = $.datepicker.formatDate("yy-mm-dd", new Date($('#fechaFin').val())) + " 00:00:00";
+        
         $.getJSON('/Inmueble/BuscarInmueblesSinContrato', { fechaInicio: fechaInicio, fechaFin: fechaFin }, function (resultados) {
             $('#tblInmuebles').empty();
             $.each(resultados, function (index, resultado) {
