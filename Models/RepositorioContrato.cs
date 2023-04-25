f6598bdd899dd152e4524d8ded1f3ea74f12e684
kaiserkey <kaiserkey2@gmail.com>
2023-04-24 22:08:02 -0300

    new changes

--- Models/RepositorioContrato.cs
+++ Models/RepositorioContrato.cs
@@ -235,7 +235,6 @@ public class RepositorioContrato
 
         using (var cmd = mySqlDatabase.Connection.CreateCommand() as MySqlCommand)
         {
-
             cmd.CommandText = @"SELECT c.IdContrato, c.IdInquilino, c.IdInmueble, c.FechaInicio, c.FechaFin,
                         i.Nombre, i.Apellido, i.Dni
                         FROM Contrato c
