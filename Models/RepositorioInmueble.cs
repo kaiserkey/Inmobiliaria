652a344d39a87fffa4e2c3fa9f9737c83d0807f6
kaiserkey <kaiserkey2@gmail.com>
2023-04-25 07:30:15 -0300

    new changes

--- Models/RepositorioInmueble.cs
+++ Models/RepositorioInmueble.cs
@@ -240,8 +240,8 @@ public class RepositorioInmueble
                                 WHERE i.Activo = 1 AND i.IdInmueble NOT IN (
                                     SELECT c.IdInmueble
                                     FROM Contrato c
-                                    WHERE (c.FechaInicio BETWEEN fechaInicio AND <fecha_fin>)
-                                        OR (c.FechaFin BETWEEN <fecha_inicio> AND <fecha_fin>)
+                                    WHERE (c.FechaInicio BETWEEN @fechaInicio AND <fecha_fin>)
+                                        OR (c.FechaFin BETWEEN fechaInicio AND <fecha_fin>)
                                 )";
             cmd.Parameters.AddWithValue("@busqueda", "%" + busqueda + "%");
             
