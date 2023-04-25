bf0effb6dc3a3298ca156e34aa55574786c4fb60
kaiserkey <kaiserkey2@gmail.com>
2023-04-25 09:59:29 -0300

    new changes

--- Controllers/ContratoController.cs
+++ Controllers/ContratoController.cs
@@ -212,7 +212,7 @@ namespace Inmobiliaria.Controllers
             try
             {
                 int res = RepoContrato.UpdateContrato(con, updateContrato);
-                Console
+                Console.WriteLine(res);
                 if(res > 0){
                     TempData["Mensaje"] = "La entidad se actualizo correctamente ID:" + id;
                 }else if (res == -1){
